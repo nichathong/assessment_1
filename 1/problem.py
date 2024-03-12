614ed2987d3a52bfd29bae0f07d9f128de9e8841
nichathong <nicha.fn@gmail.com>
2024-03-12 16:48:38 -0700

    Solved 1st problems

--- 1/problem.py
+++ 1/problem.py
@@ -1,4 +1,14 @@
 class Solution:
     def findID(self, user_IDs, id):
-        # Remove pass and write code here
-        pass
+        left, right = 0, len(user_IDs) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if user_IDs[mid] == id:
+                return mid
+            elif user_IDs[mid] < id:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+            
