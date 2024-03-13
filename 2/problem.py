e1d0a6efcef783c3c0742269e23fec0253bd0837
nichathong <nicha.fn@gmail.com>
2024-03-12 17:06:03 -0700

    Solved 2nd problems

--- 2/problem.py
+++ 2/problem.py
@@ -1,4 +1,13 @@
 class Solution:
     def planVacation(self, miles, target):
-        # Remove pass and write code here
-        pass
+        n = len(miles)
+        min_length = n + 1
+        left, sum_miles = 0, 0
+
+        for right in range(n):
+            sum_miles +=  miles[right]
+            while sum_miles >= target:
+                min_length = min(min_length, right - left + 1)
+                sum_miles -= miles[left]
+                left += 1
+        return min_length if min_length <= n else 0
